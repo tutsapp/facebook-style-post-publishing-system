07c801193bd2a8c69595477b94db8da0a09d113a
TutsApp Labs <negi.webdeveloper1@gmail.com>
2014-05-07 09:55:22 +0530

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+//file:dbconnect.php
+//dev: Rahul Negi
+//dev url: www.infotheme.com
+
+$con=@mysqli_connect("localhost","username","rahul","datatbasename");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
